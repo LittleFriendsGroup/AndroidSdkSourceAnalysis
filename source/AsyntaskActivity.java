07b8c69851b18403c51fd24a97207f9e2488678b
white37 <89284709@qq.com>
2016-04-25 17:35:51 +0800

    Update and rename AsyntaskActivity to AsyntaskActivity.java

--- source/AsyntaskActivity.java
+++ source/AsyntaskActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.yanbober.customerviewdemo.R;
 
 public class AsyntaskActivity extends AppCompatActivity {
 
